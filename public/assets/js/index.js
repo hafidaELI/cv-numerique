85e350bda4968920f45ae0844e76f4b3e9d5c0b8
hafidaELI <elidrissi-hafida@hotmail.fr>
2025-01-22 22:43:13 +0100

    modif url de partage

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -31,7 +31,7 @@ function shareProfile() {
         navigator.share({
             title: 'Mon Profil',
             text: 'Découvrez mon profil !',
-            url: 'www.elidrissi-hafida.com',
+            url: 'https://www.elidrissi-hafida.com',
         }).then(() => {
             console.log('Profil partagé avec succès !');
         }).catch((error) => {
